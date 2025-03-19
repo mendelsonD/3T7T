f730833cfb867d1b6f7353509ba031f604b13877
Daniel Mendelson - B. Bernhardt Lab <danielme@bic.mni.mcgill.ca>
2025-03-19 11:17:54 -0400

    Restructure

---
+++ analyses/xtremeVrtx.py
@@ -0,0 +1,199 @@
+# Code to  quantify extreme values on vertices along a surface.
+# Made to determine number of adjacent vertices with zBrains scores above a threshold and count number of groups of extreme values.
+
+def load_gifti(path_gii, extract = "vertices"):
+    """
+    Load data from GIFTI (.gii) file.
+    
+    Parameters:
+        path_gii : str
+            Path to the `.gii` file. Intended for `surf.gii` and `func.gii` files.
+                
+                .surf.gii : Array with two elements
+                    Vertex indexed array. Retrieved with `NIFTI_INTENT_POINTSET` option. Structure:
+                        nVertices x 3 [x, y, z] 
+                        coordinates are in unit `milimeter` (mm)
+
+                    Face indexed array. Retrieved with `NIFTI_INTENT_TRIANGLE` option. Structure:
+                        nFaces x 3 [vertexIndex1, vertexIndex2, vertexIndex3]
+                            Notes: 
+                                `vertexIndex` corresponds to the index in the above `vertex indexed array`.
+                                vertices that make each face are adjacent.
+                
+                .func.gii : Array with a single element. Structure:
+                    nVertices x [value]
+    
+    Returns:
+        gii : np.array
+            gii from the file.
+
+    Requires: 
+        nibabel as nib    
+        
+    """
+
+    import nibabel as nib
+    
+    gii = nib.load(path_gii)
+
+    if path_gii.endswith(".func.gii"):
+           return gii.darrays[0].data
+
+    elif path_gii.endswith(".surf.gii"):
+        
+        if extract == "vertices":
+            print ("[load_gifti] Extracting vertex coordinates from %s" % path_gii)
+            return gii.get_arrays_from_intent('NIFTI_INTENT_POINTSET')[0].data # vertex format
+            
+        elif extract == "faces":
+            print ("[load_gifti] Extracting faces from %s" % path_gii)
+            return gii.get_arrays_from_intent('NIFTI_INTENT_TRIANGLE')[0].data # face format
+        
+        else:
+            raise ValueError("[load_gifti] `Extract` must be either 'vertices' or 'faces'")
+    
+    else:
+        raise ValueError("[load_gifti] File type not supported. Supported types are `.surf.gii` and `.func.gii`")
+
+    
+def v_extremeValue(path_gii, threshold, output = "indices"):
+    """
+    Returns the number of vertices with values more extreme than a threshold.
+
+    Parameters:
+        path_gii : str
+            Path to the `.func.gii` file.
+        threshold : float
+            Threshold for extreme values.
+
+    Returns:
+        int : Number of vertices with values more extreme than the threshold.
+    
+    Future:
+        Can specify if want one sided or two sided threshold. Default, assumes two sided.
+
+    Requires:
+        numpy as np
+    """
+
+    import numpy as np
+
+    try:
+        threshold = float(threshold)
+    except ValueError:
+        raise ValueError("Threshold must be a positive float or integer.")
+
+    values = load_gifti(path_gii)
+
+    if output == "values": # for .func.gii
+        out = values[np.abs(values) > np.abs(threshold)]
+        print("[v_extremeValue] Returning values only, no indices.")
+
+    elif output == "indices":
+        out = [index for index in range(len(values)) if np.abs(values[index]) > np.abs(threshold)]
+        print("[v_extremeValue] Returning indices only, no values.")
+
+    elif output == "both":
+        out = (values[np.abs(values) > np.abs(threshold)], [index for index in range(len(values)) if np.abs(values[index]) > np.abs(threshold)])
+        print("[v_extremeValue] Returning both values and indices.")
+    else:
+        raise ValueError("[v_extremeValue] Output parameter illdefined. It is currently %s Must be either `values`, `indices` or `both`." % output)
+
+    return out
+
+def adjVertices(path_surf_gii, listVertices, test = False):
+    """
+    Identifies sets of adjacent vertices in a list of vertices of interest.
+
+    Parameters:
+        path_surf_gii : str
+            Path to `.surf.gii` file with surface face information
+        vertOfInterest : list
+            List of vertices of interest.
+
+
+    Returns:
+        listAdjSets : list
+            List of list of adjacent vertices. Each nested 
+    """
+
+    import nibabel as nib
+    import numpy as np
+
+    def run(faces, v, listVertices, set = [], listAdjSets = []):
+        
+        import numpy as np
+
+        # print("VERTEX: %s" %(v))
+        overlap = [s for s in listAdjSets if v in s] # if `v` is in any set of listAdjSets, saves overlapping set(s)
+            
+        if len(overlap) > 1:
+            raise ValueError("[adjVertices] Vertex %s is in more than one set. This should not happen. Examine code." %v)
+        
+        if overlap: # if `v` is in a set, remove that set from listAdjSets 
+            # print(f"\tVertex %s is in set %s" %(v, overlap))
+            set = overlap[0] # define set as the set that `v` is in
+            listAdjSets.remove(overlap[0]) # remove that set from listAdjSets
+
+        else: # add `v` to a new set and continue
+            set = [v] # define new set with v
+
+        #print("Set: %s" %set)
+        
+        # find adjacent vertices to `v`
+        adjV = np.unique(faces[np.any(faces == v, axis=1)]) # 1D array
+
+        for i in adjV: # Identify adjacent vertices that are also of interest 
+            if i in listVertices:
+                #print(f"\t %s" %i)
+                if i in set: # if in the current set, skip
+                    #print(f"\t%s\tIn current set %s. Skipping." %(i, set))
+                    continue
+
+                elif any(i in s for s in listAdjSets): # if in another set, combine current set with that set
+                    overlap = [s for s in listAdjSets if i in s]
+                    
+                    if len(overlap) > 1:
+                        raise ValueError("[adjVertices] Vertex %s is in more than one set. This should not happen. Examine code." %i)
+                    
+                    #print(f"\t%s\tIn another set. Combining current set %s with %s." %(i, set, set + i))
+
+                    old = overlap[0]
+                    set = set + old
+                    #print(type(set))
+                    listAdjSets.remove(old)
+
+                    continue
+            
+                else: # not in any set, add to the current set
+                    #print(f"\t%s\tNew. Appending to current set %s." %(i, set))
+                    set.append(i)
+                    continue
+            
+            else:
+                continue
+
+        listAdjSets.append(set)
+        #print("List of sets: %s" %listAdjSets)
+
+        return listAdjSets
+
+    faces = load_gifti(path_surf_gii, extract = "faces")
+    
+    set = [] # list of a single set of adjacent vertices
+    listAdjSets = [] # list of sets of adjacent vertices
+
+    if test == True:
+            print("[adjVertices] Testing for first 10 vertices of interest only.")
+            
+            for v in listVertices[:40]:
+                listAdjSets = run(faces, v, listVertices, set, listAdjSets)
+                
+    else:
+        for v in listVertices:
+            listAdjSets = run(faces, v, listVertices, set, listAdjSets)
+
+    print("[adjVertices] COMPLETE. Num sets: %s, Max length: %s, Longest set: %s, List of sets: %s" %(len(listAdjSets), max(map(len, listAdjSets)), max(listAdjSets, key=len), listAdjSets))
+    # order listAdjSets by length of sets
+    listAdjSets.sort(key = len, reverse = True)
+    return listAdjSets
